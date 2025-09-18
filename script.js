5583b8785a85f39513b9c3d40f4afc7533a7e0d9
Dante López <danterrlopez@gmail.com>
2025-09-18 13:04:23 -0600

    Laboratorio ABC realizado por MotionMakers

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-/* Comentario multilínea */
-//? Comentario de 1 línea
 
-const primerHola = document.querySelector('#lab h1');
-if (primerHola) {
-  primerHola.textContent = 'Adiós';
-}
+        
+        function getRandomColor() {
+        const colors = ['green', 'blue', 'red'];
+          const randomIndex = Math.floor(Math.random() * colors.length);
+        return colors[randomIndex];
+        }
 
-const h1Naranja = document.getElementById('naranja');
-if (h1Naranja) {
-  h1Naranja.style.color = 'orange';
-}
-
-const h1Marron = document.getElementById('cambia-marron');
-if (h1Marron) {
-  h1Marron.style.cursor = 'pointer';       
-  h1Marron.addEventListener('click', () => {
-    h1Marron.style.color = 'brown';
-  });
-}
-
+document.addEventListener('DOMContentLoaded', () => {
+    const h5Elements = document.querySelectorAll('h5');
+    h5Elements.forEach(h5 => {
+        h5.addEventListener('click', (event) => {
+            event.target.style.color = getRandomColor();
+        });
+    });
+});
